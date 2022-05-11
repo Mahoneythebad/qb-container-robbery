115f95d369b7ce8e3b31a4a267462072101d3c6b
Mahoneythebad <91486334+Mahoneythebad@users.noreply.github.com>
2022-05-11 20:06:04 +0200

    Delete shared.lua

--- shared.lua
+++
@@ -1,4 +0,0 @@
---Új itemek (Container Robbery)
-
-['hardcutter']						= {['name'] = 'hardcutter',					['label'] = 'Hardcutter',					['weight'] = 100, 		['type'] = 'item', 		['image'] = 'hardcutter.png',		['unique'] = true, 		['useable'] = true,		['shouldClose'] = true,		 ['combinable'] = nil, 	 	 	["decay"] = 10.0,     ["created"] = nil,      ['description'] = '...'},
-    
