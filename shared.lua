236622d85e39771b7c6ff99933e5f6c31316fd38
Mahoneythebad <91486334+Mahoneythebad@users.noreply.github.com>
2022-05-11 20:04:58 +0200

    Add files via upload

---
+++ shared.lua
@@ -0,0 +1,4 @@
+--Új itemek (Container Robbery)
+
+['hardcutter']						= {['name'] = 'hardcutter',					['label'] = 'Hardcutter',					['weight'] = 100, 		['type'] = 'item', 		['image'] = 'hardcutter.png',		['unique'] = true, 		['useable'] = true,		['shouldClose'] = true,		 ['combinable'] = nil, 	 	 	["decay"] = 10.0,     ["created"] = nil,      ['description'] = '...'},
+    
